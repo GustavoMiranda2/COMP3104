477d978962c478e96b5ea73779233067a39d2d2e
GustavoMiranda2 <101488574@georgebrown.ca>
2025-10-05 22:46:07 -0400

    add public and src folders with starter files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to COMP3104 Lab</h1>
+      <p>Student Name: Gustavo Miranda</p>
+      <p>Student ID: 101488574</p>
+    </div>
+  );
+}
+
+export default App;
